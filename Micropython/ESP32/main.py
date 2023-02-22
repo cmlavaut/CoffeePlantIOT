9b15bcb36c2afae33cc3a7ebc5909d3e9e074db4
Camila <cmlavaut96@gmail.com>
2023-02-21 23:40:52 -0600

    arreglado

--- Micropython/ESP32/main.py
+++ Micropython/ESP32/main.py
@@ -2,26 +2,26 @@
 import dht
 from machine import Pin, ADC,UART
 from time import sleep
+import led_parpadeo
 
-sensor11 = dht.DHT11(Pin(27))
-#sensor22 = dht.DHT22(Pin(26))
-sensor = ADC(Pin(12))
+sensor22 = dht.DHT22(Pin(27))
+sensor = ADC(Pin(14))
 sensor.atten(ADC.ATTN_11DB)
 uart = UART(2,baudrate=115200)
-
+max_suelo = 54525
+min_suelo = 22005
 
 while True:
-    valor = sensor.read_u16()
-    print(valor)
-    sensor11.measure()
-    sensor11_temp =  sensor11.temperature()
-    sensor11_hum = sensor11.humidity()
-    print("temp amb: {} C y humedad relativa {} %".format(sensor11_temp,sensor11_hum))
+    valor = (max_suelo - sensor.read_u16())*100/(max_suelo-min_suelo)
+    if valor < 50:
+        led_parpadeo.encendido(25)
+    else:
+        led_parpadeo.encendido(26)
     sensor22.measure()
     sensor22_temp =  sensor22.temperature()
     sensor22_hum = sensor22.humidity()
-    print("temp amb: {} C y humedad relativa {} %".format(sensor22_temp, sensor22_hum))
-    uart.write("{},{},{},{},{}\n".format(valor,sensor11_temp, sensor11_hum,sensor22_temp, sensor22_hum) )
+    print("maceta cuarto: {}% {}% {}C".format(valor,sensor22_hum,sensor22_temp))
+    uart.write("maceta cuarto: {}% {}% {}C \n".format(valor,sensor22_hum, sensor22_temp))
     sleep(2)
     
     
