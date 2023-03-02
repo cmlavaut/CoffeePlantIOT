31df06d0f437e59a2e008973d1c2e0106d593e08
RASP-KMI <cmlavaut96@gmail.com>
2023-03-02 15:58:41 -0600

    clase8

--- filtrarcuarto.py
+++ filtrarcuarto.py
@@ -1,28 +1,26 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import sys
+
+path = "./csv/datos.csv"
 
 def main():
-    datos = pd.read_csv("./datos.csv")
-    cuarto = datos[(datos["Place"]=="cuarto")&(datos["Fecha"]=="26 02 23")].reset_index(drop=True)
-    cuarto = cuarto.loc[:,["Hora", "humedad_suelo", "humedad_amb"]]
+    place = sys.argv[1]
+    date = sys.argv[2]
+    parametro1 = sys.argv[3]
+    datos = pd.read_csv(path)
+    cuarto = datos[(datos["Place"]== place )&(datos["Fecha"]== date)].reset_index(drop=True)
+    cuarto = cuarto.loc[:,["Hora", parametro1]]
     contador = 0
     for index, row in cuarto.iterrows():
-        cuarto.loc[contador,"humedad_suelo"] = float(row["humedad_suelo"][:-1])
-        cuarto.loc[contador,"humedad_amb"] = float(row["humedad_amb"][:-1])
+        cuarto.loc[contador,parametro1] = float(row[parametro1][:-1])
         contador += 1 
     print("valor max")
-    valor = cuarto.loc[:,"humedad_suelo"].max()
-    print(cuarto[cuarto["humedad_suelo"] == valor])
+    valor = cuarto.loc[:, parametro1].max()
+    print(cuarto[cuarto[parametro1] == valor])
     
 
-    print("valor min")
-    valor = cuarto.loc[:,"humedad_suelo"].min()
-    print(cuarto[cuarto["humedad_suelo"] == valor])
-     
-    #sala = sala[sala["temperatura"] < 35] 
-    #print(sala)
-    #cuarto.plot(x= "Hora", y1= "humedad_suelo", y2 = "humedad_amb")
-    cuarto.plot(x= "Hora", y= "humedad_amb")
+    cuarto.plot(x= "Hora", y= parametro1)
     plt.show()
 
 if __name__ == "__main__":
--- lectura+guardar.py
+++ lectura+guardar.py
@@ -4,95 +4,64 @@ import sys
 import pandas as pd
 from datetime import datetime
 
-'''
-dicc = {
-        "Place" : [],
-        "humedad_suelo" : [],
-        "humedad_amb" : [],
-        "temperatura" :[],
-        }
-tabla = pd.DataFrame.from_dict(dicc)
-tabla.to_csv('./datos.csv',index= False)
-'''
-def main():
-    arduino = serial.Serial()
-    arduino2= serial.Serial()
-    try:
-        puerto1= sys.argv[1]
-        puerto2= sys.argv[2]
-        arduino.port= 'COM' + puerto1
-        arduino2.port = 'COM' + puerto2
-        arduino.baudrate = 115200
-        arduino2.baudrate = 115200
-        arduino.open()
-        arduino2.open()
-    except:
-        try:
-            #puerto1 = sys.argv[1]
-            #puerto2 = sys.argv[2]
-            arduino.port = "/dev/ttyUSB0" 
-            arduino2.port = "/dev/ttyUSB1"
-            arduino.baudrate = 115200
-            arduino2.baudrate = 115200
-            arduino.open()
-            arduino2.open()
-        except:
-            print("nothing conected")
-            quit()
-    
-    while True:
-        arduino.flushInput()
-        sensor = arduino.readline()
-        sensor = sensor.decode()
-        value= sensor.split()
-        arduino2.flushInput()
-        sensor11 = arduino2.readline()
-        sensor11 = sensor11.decode()
-        value11 = sensor11.split()
-        print(value)
-        print(value11)
-        if (len(value)==4 and len(value11)==4):
-            print("valores correctos")
-            guardar(value, value11)
-            #print(tabla)
-        else:
-            print("valores incorrectos")
-        
-        time.sleep(3)
-    arduino.close()
- 
+path = './csv/datos.csv'
 
 
-
-def guardar(valorA, valorB):
-        tabla = pd.read_csv('./datos.csv')
+def guardar(valorA,tabla):
         now = datetime.now()
         now_fecha = now.strftime("%d %m %y")
         now_hora = now.strftime("%H:%M:%S")
-        ciclo =0
         try:
             tabla.insert(0, "Fecha", now_fecha)
             tabla.insert(1, "Hora", now_hora)
         except:
             #print("ya existe columnas de hora y fecha")
-            pass
-               
+            pass            
+        datos = [now_fecha, now_hora]
+        datos = datos + valorA
+        tabla.loc[tabla.shape[0]] = datos
+        tabla.to_csv(path, index = False)
+        print(tabla)
 
-        for contador in range(2):
-            if ciclo == 0:
-                datos = [now_fecha, now_hora]
-                lista = valorA
-            elif ciclo ==1:
-                datos = [now_fecha, now_hora]
-                lista = valorB
-            for i in range(len(lista)):
-                datos.append(lista[i])
-            tabla.loc[tabla.shape[0]] = datos
-            tabla.to_csv('./datos.csv', index = False)
-            ciclo =1 
-            
 
-        return tabla
+def main():
+    #Leeer datos anteriores
+    try:
+        tabla = pd.read_csv(path)
+    except:
+        dicc = {
+            "Place" : [],
+            "humedad_suelo" : [],
+            "humedad_amb" : [],
+            "temperatura" :[],
+        }
+        tabla = pd.DataFrame.from_dict(dicc)
+        tabla.to_csv(path,index= False)
+    #Comunciacion Serial
+    arduino = serial.Serial()
+    try:
+        puerto= sys.argv[1]
+        arduino.port= puerto
+        arduino.baudrate = 115200
+        arduino.open()
+    except:
+        print("nothing conected")
+        quit()
+    
+    arduino.flushInput()
+    sensor = arduino.readline()
+    sensor = sensor.decode()
+    value= sensor.split()
+    #print(value)
+    if (len(value)==4):
+        print("valores correctos")
+        guardar(value,tabla)
+    else:
+        print("valores incorrectos")
+        arduino.close()
+        main()
+    arduino.close()
+
 
 if __name__ == "__main__":
     main()
