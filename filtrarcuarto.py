37eb08013180f255ce1af0fcca3066b9b22a52a9
RASP-KMI <cmlavaut96@gmail.com>
2023-03-09 01:19:32 -0600

    new

--- filtrarcuarto.py
+++ filtrarcuarto.py
@@ -2,27 +2,26 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import sys
 
-path = "./csv/datos.csv"
+path = "./csv/datos_new.csv"
 
 def main():
     place = sys.argv[1]
     date = sys.argv[2]
-    parametro1 = sys.argv[3]
+    columna = sys.argv[3]
     datos = pd.read_csv(path)
     cuarto = datos[(datos["Place"]== place )&(datos["Fecha"]== date)].reset_index(drop=True)
-    cuarto = cuarto.loc[:,["Hora", parametro1]]
+    cuarto = cuarto.loc[:,["Hora", columna]]
+    
     contador = 0
     for index, row in cuarto.iterrows():
-        cuarto.loc[contador,parametro1] = float(row[parametro1][:-1])
+        cuarto.loc[contador,columna] = float(row[columna][:])
+        print(row[columna])
         contador += 1 
     print("valor max")
-    valor = cuarto.loc[:, parametro1].max()
-    print(cuarto[cuarto[parametro1] == valor])
+    valor = cuarto.loc[:, columna].max()
+    print(cuarto[cuarto[columna] == valor])
     
-
-    cuarto.plot(x= "Hora", y= parametro1)
-    plt.show()
-
+    
 if __name__ == "__main__":
     main()
 
--- graficar.py
+++ graficar.py
@@ -1,8 +1,9 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import sys
 
-path = "./csv/datos.csv"
+path = "./csv/datos_new.csv"
 hora_arreglo = np.arange(0,24)
 humedadamb = np.empty(0)
 humedadsuelo = np.empty(0)
@@ -20,12 +21,14 @@ def promedio(columna,i , sala):
 def main():
     global humedadamb, humedadsuelo
     datos = pd.read_csv(path)
-    sala = datos[(datos["Place"]=="sala")&(datos["Fecha"]=="27 02 23")].reset_index(drop=True)
-        
+    place = sys.argv[1]
+    date = sys.argv[2]
+    sala = datos[(datos["Place"]==place)&(datos["Fecha"]==date)].reset_index(drop=True)
+    print
     
     for index, row in sala.iterrows():
-        sala.loc[index,"humedad_amb"] = row["humedad_amb"][:-1]
-        sala.loc[index,"humedad_suelo"] = row["humedad_suelo"][:-1]
+        sala.loc[index,"humedad_amb"] = row["humedad_amb"][:]
+        sala.loc[index,"humedad_suelo"] = row["humedad_suelo"][:]
         sala.loc[index,"Hora"] = row["Hora"][:-6]
     
     print(sala.tail(8)) #muestra los ultimos 8 filas
@@ -39,10 +42,19 @@ def main():
     print(hora_arreglo)
     print(humedadamb)
     print(humedadsuelo)
-        
-    plt.plot(hora_arreglo, humedadamb)
-    plt.plot(hora_arreglo, humedadsuelo)
+       
+    plt.subplot(2,1,1)
+    plt.plot(hora_arreglo, humedadamb, "r", marker = "o")
+    plt.xlabel("time")
+    plt.ylabel("humedad ambiental")
+    plt.title("Sensores")
+    plt.subplot(2,1,2)
+    plt.plot(hora_arreglo, humedadsuelo, "m", marker = "o")
+    #plt.scatter(hora_arreglo, humedadsuelo)
+    plt.xlabel("time")
+    plt.ylabel("humedad suelo")
     plt.show()
+    
 
 if __name__ == "__main__":
     main()
