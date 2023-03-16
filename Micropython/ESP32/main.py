873c891b62598a229ab44e8fe5e4a9526d9e7bc4
Camila <cmlavaut96@gmail.com>
2023-03-16 14:51:19 -0600

    tarea10

--- Micropython/ESP32/main.py
+++ Micropython/ESP32/main.py
@@ -1,27 +1,58 @@
+
 #microcontrolador ESP32
 import dht
 from machine import Pin, ADC,UART
 from time import sleep
 import led_parpadeo
+from umqtt.robust import MQTTClient
+import machine
+import network
 
-sensor11 = dht.DHT11(Pin(27))
-sensor = ADC(Pin(12))
+ssid = "N-47"
+password = "@victor47"
+
+broker = "192.168.50.168"
+topic = "sensores/cuarto"
+
+sensor22 = dht.DHT22(Pin(27))
+sensor = ADC(Pin(34))
 sensor.atten(ADC.ATTN_11DB)
-uart = UART(2,baudrate=115200)
 max_suelo = 54525
 min_suelo = 22005
+sensornivel = Pin(2,Pin.IN,Pin.PULL_DOWN)
+motor = Pin(23,Pin.OUT)
 
+def connect_wifi():
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    if not wlan.isconnected():
+        print("por favor conectar")
+        wlan.connect(ssid, password)
+        while not wlan.isconnected():
+            pass
+    print("conexion establecida: ", wlan.ifconfig())
+
+def publicar_MQTT(mensaje):
+    client = MQTTClient("esp32", broker)
+    client.connect()
+    client.publish(topic, mensaje)
+    client.disconnect()
+    
+connect_wifi()
 while True:
     valor = (max_suelo - sensor.read_u16())*100/(max_suelo-min_suelo)
-    if valor < 50:
-        led_parpadeo.encendido(32)
+    if not sensornivel.value():
+        motor.off()
+    elif valor < 50:
+        motor.on()
+        led_parpadeo.encendido(18)
     else:
-        led_parpadeo.encendido(33)
-    sensor11.measure()
-    sensor11_temp =  sensor11.temperature()
-    sensor11_hum = sensor11.humidity()
-    print("cuarto {} {} {}".format(valor,sensor11_hum,sensor11_temp))
-    uart.write("cuarto {} {} {} \n".format(valor,sensor11_hum, sensor11_temp))
+        motor.off()
+        led_parpadeo.encendido(19)
+    sensor22.measure()
+    sensor22_temp =  sensor22.temperature()
+    sensor22_hum = sensor22.humidity()
+    mensaje = "cuarto {} {} {}".format(valor,sensor22_hum,sensor22_temp)
+    print(mensaje)
+    publicar_MQTT(mensaje)
     sleep(2)
-    
-    
