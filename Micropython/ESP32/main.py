2bef10408f84dbb9b64d3f77504e32fdabc0dad1
Camila <124115184+cmlavaut@users.noreply.github.com>
2023-03-21 23:18:05 -0600

    Update main.py

--- Micropython/ESP32/main.py
+++ Micropython/ESP32/main.py
@@ -1,33 +1,33 @@
-
 #microcontrolador ESP32
 import dht
-from machine import Pin, ADC,UART
+from machine import Pin, ADC
 from time import sleep
 import led_parpadeo
 from umqtt.robust import MQTTClient
 import machine
 import network
+import uos
 import _thread
 
 ssid = "N-47"
 password = "@victor47"
 
 broker = "192.168.50.168"
-topic = "sensores/cuarto"
+topic = "sensores"
 
 sensorHyT = dht.DHT22(Pin(27))
 sensorHumedad = ADC(Pin(34))
 sensorHumedad.atten(ADC.ATTN_11DB)
 max_suelo = 54525
 min_suelo = 22005
-<<<<<<< HEAD
 sensornivel = ADC(Pin(35))
 sensornivel.atten(ADC.ATTN_11DB)
-=======
-sensornivel = Pin(4,Pin.IN)
->>>>>>> eb71aad6a7cb39b28b848b76e1d4ad61fbe3bed0
 motor = Pin(23,Pin.OUT)
-valor = 0
+valorHumedad = 0
+leerHumedad = open("humedad.txt", mode='r')
+leerTiempo = open("tiempo.txt", mode='r')
+humedadMinima = int(leerHumedad.read())
+tiempo = int(leerTiempo.read())
 agua = 0
 
 def connect_wifi():
@@ -40,61 +40,63 @@ def connect_wifi():
             pass
     print("conexion establecida: ", wlan.ifconfig())
 
-def publicar_MQTT(mensaje):
-    client = MQTTClient("esp32", broker)
-    client.connect()
-    client.publish(topic, mensaje)
-    client.disconnect()
-
 def regar():
-    global sensornivel,motor,valor,agua
+    global sensornivel,motor,valorHumedad,agua,humedadMinima,tiempo
     while True:
         sleep(0.5)
-        print(sensornivel.read_u16())
+        valorHumedad = (max_suelo - sensorHumedad.read_u16())*100/(max_suelo-min_suelo)
+        #print(sensornivel.read_u16())
         if sensornivel.read_u16() < 401:
             motor.off()
             agua = 0
-        elif valor < 50:
-            motor.on()
+        elif valorHumedad < humedadMinima:
             led_parpadeo.encendido(18)
+            motor.on()
+            sleep(tiempo)
+            motor.off()
+            sleep(0.5)
             agua = 1
         else:
             motor.off()
             agua = 1
             led_parpadeo.encendido(19)
-    
+            
+            
+def setHumedadMinima(topic, msg):
+    global humedadMinima,tiempo
+    if topic == b"control":
+        try:
+            humedadMinima = int(msg)
+        except:
+            datos = msg.decode().split(',')
+            print(datos)
+            humedadMinima = int(datos[0])
+            tiempo = int(datos[1])
+            with open("humedad.txt", "w") as f:
+                f.write(datos[0])
+            with open("tiempo.txt", "w") as f:
+                f.write(datos[1])
+            
 def mqttPublicar():
-    global valor,sensorHumedad,agua,sensorHyT 
+    global valorHumedad,sensorHumedad,agua,sensorHyT, motor,humedadMinima,tiempo,sensornivel
+    client = MQTTClient("esp32", broker)
+    client.set_callback(setHumedadMinima)
+    client.connect()
+    client.subscribe(topic="control")
     while True:
-        valor = (max_suelo - sensorHumedad.read_u16())*100/(max_suelo-min_suelo)
-        sensorHyT.measure()
-        temp =  sensorHyT.temperature()
-        hum = sensorHyT.humidity()
-        mensaje = "cuarto {} {} {} {}".format(valor,hum,temp,agua)
-        print(mensaje)
-        publicar_MQTT(mensaje)
+        if not motor.value():
+            sensorHyT.measure()
+            temp =  sensorHyT.temperature()
+            hum = sensorHyT.humidity()
+            mensaje = "00 {} {} {} {} {} {}".format(valorHumedad,humedadMinima,tiempo,hum,temp, agua)
+            print(mensaje)
+        else :
+            mensaje = "01 {} {} {} {}".format(valorHumedad,humedadMinima,tiempo,agua)
+            print(mensaje)
+        client.publish(topic, mensaje)
+        client.check_msg()
         sleep(2)
         
 connect_wifi()
-<<<<<<< HEAD
 _thread.start_new_thread(regar, ())
 _thread.start_new_thread(mqttPublicar, ())
-=======
-while True:
-    valor = (max_suelo - sensor.read_u16())*100/(max_suelo-min_suelo)
-    if not sensornivel.value():
-        motor.off()
-    elif valor < 50:
-        motor.on()
-        led_parpadeo.encendido(18)
-    else:
-        motor.off()
-        led_parpadeo.encendido(19)
-    sensor22.measure()
-    sensor22_temp =  sensor22.temperature()
-    sensor22_hum = sensor22.humidity()
-    mensaje = "cuarto {} {} {}".format(valor,sensor22_hum,sensor22_temp)
-    print(mensaje)
-    publicar_MQTT(mensaje)
-    sleep(2)
->>>>>>> eb71aad6a7cb39b28b848b76e1d4ad61fbe3bed0
