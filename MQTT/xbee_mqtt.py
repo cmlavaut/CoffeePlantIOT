7e3ee986fe089b2c5cae1450c1d24caf501b9160
RASP-KMI <cmlavaut96@gmail.com>
2023-03-27 22:31:06 -0600

    tarea11

--- MQTT/xbee_mqtt.py
+++ MQTT/xbee_mqtt.py
@@ -1,15 +1,73 @@
-import serial 
+import serial
+import time
+import sys
+import pandas as pd
+from datetime import datetime
 import paho.mqtt.client as mqtt
 
+path = '/home/kmi/CoffeePlantIOT/csv/mediciones.csv'
+broker = '192.168.0.20'
 
-ser = serial.Serial('/dev/ttyUSB1', 115200)
-client = mqtt.Client()
-client.connect('192.168.0.20', 1883)
+def guardar(valorA,tabla):
+        now = datetime.now()
+        now_fecha = now.strftime("%d %m %y")
+        now_hora = now.strftime("%H:%M:%S")
+        try:
+            tabla.insert(0, "Fecha", now_fecha)
+            tabla.insert(1, "Hora", now_hora)
+        except:
+            #print("ya existe columnas de hora y fecha")
+            pass            
+        datos = [now_fecha, now_hora]
+        datos = datos + valorA
+        tabla.loc[tabla.shape[0]] = datos
+        tabla.to_csv(path, index = False)
+        print(tabla)
 
-while True:
-    data = ser.readline().strip()
-    print(data)
-    client.publish('datos/xbee', data)
+
+def main():
+    #Leeer datos anteriores
+    try:
+        tabla = pd.read_csv(path)
+    except:
+        dicc = {
+            "Place" : [],
+            "humedad_suelo" : [],
+            "humedad_amb" : [],
+            "temperatura" :[],
+            "status_agua" : [],
+        }
+        tabla = pd.DataFrame.from_dict(dicc)
+        tabla.to_csv(path,index= False)
+    #Comunciacion Serial
+    arduino = serial.Serial()
+    try:
+        puerto= sys.argv[1]
+        arduino.port= puerto
+        arduino.baudrate = 115200
+        topic = sys.argv[2]
+        arduino.open()
+    except:
+        print("nothing conected")
+        quit()
+    
+    arduino.flushInput()
+    client = mqtt.Client()
+    client.connect(broker, 1883)
+    sensor = arduino.readline()
+    client.publish(topic, sensor)
+    sensor = sensor.decode()
+    value= sensor.split()
+    #print(value)
+    if (len(value)==5):
+        print("valores correctos")
+        guardar(value,tabla)
+    else:
+        print("valores incorrectos")
+        arduino.close()
+        main()
+    arduino.close()
 
 
-
+if __name__ == "__main__":
+    main()
